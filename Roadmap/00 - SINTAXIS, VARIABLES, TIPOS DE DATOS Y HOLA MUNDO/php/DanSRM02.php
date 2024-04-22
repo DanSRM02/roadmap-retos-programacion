50251bd9bd926605ab1428dbcd36220693df5e8f
Daniel Ramirez <167477641+DanSRM02@users.noreply.github.com>
2024-04-22 10:55:52 +0000

    Variables primarias de PHP y comentarios...

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/php/DanSRM02.php
@@ -0,0 +1,25 @@
+<?php 
+  // https://www.php.net <- Web Oficial 
+
+  # Este comentario es de una línea  
+  
+  /*    
+    Este comentario es de varias líneas    
+  */
+  
+  // Para crear una variable en PHP se utiliza el simbolo $ y nombre de la variable siguiendo el tipado CamelCase
+  
+  $myStr = "Hola, PHP!";
+  $myNum = 30;
+  $myFloat = 3.1;
+  $myBool = true;
+  $myArray = array("Medellín", "Bogotá","Cali", "Barranquilla", "Cartagena");
+
+  echo($myStr);
+
+  // Para declarar una constante se utiliza la palabra const y el nombre de la variable siguiendo el tipado UPPER SNAKE CASE
+
+  const PI = 3.1416;
+
+
+?>
